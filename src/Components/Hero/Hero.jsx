44331a5d90c2cbdc492923ad8df76620f07c482b
Midhun Mohan <86245388+Majorshot@users.noreply.github.com>
2025-03-09 17:43:26 +0530

    Update Hero.jsx

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -7,8 +7,8 @@ const Hero = () => {
   return (
     <div id='home' className='hero'>
         <img src={profile_img} width={400} height={400} alt="" />
-        <h1><span>Im Midhun Mohan,</span> Full Stack Devoleper</h1>
-        <p>Welcome to my Porfolio, if you want to contact me or want to check my resume, just click the buttons below, Want to know more about me? Just scroll down😉</p>
+        <h1><span>I'm Midhun Mohan,</span> Full Stack Developer</h1>
+        <p>Welcome to my Porfolio,Here you can check my resume and connect with me via clicking the buttons below, Want to know more about me? Just scroll down!</p>
         <div className="hero-action">
             <div className="hero-connect"><AnchorLink className='anchor-link' offset={50} href='#contact'>Connect with me</AnchorLink></div>
             <a className="hero-resume" href='https://drive.google.com/file/d/1qYm716zhd-6dLzKDejQPemawW6MS59Of/view?usp=sharing'>My resume</a>
